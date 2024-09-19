c8a591988d8d03aa6f15cb957ed472b306d3af71
seunval <seunval@gmail.com>
2024-09-19 13:27:45 -0400

    additional cipher power

--- secret_messages.py
+++ secret_messages.py
@@ -1,4 +1,6 @@
 from tkinter import messagebox, simpledialog, Tk
+from random import choice
+
 root = Tk()
 root.withdraw()
 
@@ -32,28 +34,52 @@ def get_odd_letters(message):
     return odd_letters
 
 
-def swap_letters(message):
-    letter_list = []
-    if not is_even(len(message)):
-        message += 'x'
-    even_letters = get_even_letters(message)
-    odd_letters = get_odd_letters(message)
-    for counter in range(0, int(len(message)/2)):
-        letter_list.append(odd_letters[counter])
-        letter_list.append(even_letters[counter])
-    new_message = ''.join(letter_list)
+# def swap_letters(message):
+#     letter_list = []
+#     if not is_even(len(message)):
+#         message += 'x'
+#     even_letters = get_even_letters(message)
+#     odd_letters = get_odd_letters(message)
+#     for counter in range(0, int(len(message)/2)):
+#         letter_list.append(odd_letters[counter])
+#         letter_list.append(even_letters[counter])
+#     new_message = ''.join(letter_list)
+#     return new_message
+
+
+def encrypt(message):
+    encrypted_list = []
+    fake_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'r', 's', 't', 'u', 'v']
+    for counter in range(0, len(message)):
+        encrypted_list.append(message[counter])
+        encrypted_list.append(choice(fake_letters))
+    new_message = ''.join(encrypted_list)
     return new_message
+    # swapped_message = swap_letters(message)
+    # encrypted_message = ''.join(reversed(swapped_message))
+    # return encrypted_message
+
+
+def decrypt(message):
+    even_letters = get_even_letters(message)
+    new_message = ''.join(even_letters)
+    return new_message
+    # unreversed_message = ''.join(reversed(message))
+    # decrypted_message = swap_letters(unreversed_message)
+    # return decrypted_message
 
 
 while True:
     task = get_task()
     if task == 'encrypt':
         message = get_message()
-        encrypted = swap_letters(message)
+        # encrypted = swap_letters(message)
+        encrypted = encrypt(message)
         messagebox.showinfo('Ciphertext of the secret message is:', encrypted)
     elif task == 'decrypt':
         message = get_message()
-        decrypted = swap_letters(message)
+        # decrypted = swap_letters(message)
+        decrypted = decrypt(message)
         messagebox.showinfo('Plaintext of the secret message is:', decrypted)
     else:
         break
